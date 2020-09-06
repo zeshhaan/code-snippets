357aec2aa37732dcf273ef3e34845903e762b729
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2020-09-06 12:52:25 +0530

    Added Jquery code to hide the section if there is no cms items inside it

---
+++ speakeasy.js
@@ -0,0 +1,10 @@
+
+//Home Page - Events this Week
+// if there is nothing inside .cms collection list with class .weekevents-list
+if(!$('.weekevents-list').length){	
+
+	// then hide the whole section
+	$('.weekevents').hide();
+  
+}
+
